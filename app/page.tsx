6371959c1ee37a3769668611a3d2078627f636d9
kaschefi <kaschefirad@gmail.com>
2026-09-23 18:42:14 +0200

    logo fixed?

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
         <div className="brand">ENIGMA MAGAZINE</div>
         <div className="mast-rule" />
         <div className="university-brand">
-          <Image className="university-logo" src="/layer 6.png" alt="Computer Science Department logo" width={150} height={150} priority />
+          <Image className="university-logo" src="/layer6.png" alt="Computer Science Department logo" width={150} height={150} priority />
           <Image className="university-logo" src="/university-logo.png" alt="University of Science and Technology of Mazandaran logo" width={150} height={150} priority />
           <span><b>UNIVERSITY OF SCIENCE</b><em>&amp;</em><b>TECHNOLOGY OF MAZANDARAN</b></span>
         </div>
